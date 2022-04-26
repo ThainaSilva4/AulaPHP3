fc68bc7cf50c98736fb2f21a94b8e3b9d2f678af
Thainá Ferreira <thaina.silva254@etec.sp.gov.br>
2022-04-26 12:02:39 -0300

    Aula PHP3

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Formulário</title>
+    </head>
+	<body>
+		<h1>Cadastro Pessoa Física</h1><br>
+		<form method="GET" action="controller.php">
+			<label for="lblNome">Nome:</label><br>
+			<input type="text" name="txtNome"><br>
+			<label for="lblEndereco">Endereco:</label><br>
+			<input type="text" name="txtEndereco"><br>
+			<label for="lblBairro">Bairro:</label><br>
+			<input type="text" name="txtBairro"><br>
+			<label for="lblCep">Cep:</label><br>
+			<input type="text" name="txtCep"><br><br>
+			<button>Cadastrar</button>
+        </form>
+</body>
+</html>
